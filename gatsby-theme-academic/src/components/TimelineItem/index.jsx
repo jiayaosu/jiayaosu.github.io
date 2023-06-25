e5bb3d5995213a0473383d203ee6f15eb250f5d2
kx-huang <Huang.KX@sjtu.edu.cn>
2023-06-25 05:16:39 -0400

    update: icon-or-iamge logic

--- gatsby-theme-academic/src/components/TimelineItem/index.jsx
+++ gatsby-theme-academic/src/components/TimelineItem/index.jsx
@@ -11,7 +11,8 @@ const TimelineItem = (data) => (
     subtitleStyle={{ fontSize: '13pt', fontWeight: '400' }}
     createdAt={data.date}
     style={{ fontSize: '12pt', fontWeight: '300' }}
-    icon={<Icon size="md" fixedWidth icon={data.icon || 'school'} />}
+    // first find img file, else use icon or default
+    icon={data.icon.includes('.')?(<img src={require(`./img/${data.icon}`).default} style={{ 'border-radius': '50%', height: '75%' }} />):<Icon size="md" fixedWidth icon={data.icon || 'school'} />}
     iconStyle={{ cursor: 'default' }}
     iconColor="#44566C"
   />
